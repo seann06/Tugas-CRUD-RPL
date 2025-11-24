7f6af40e8696b994f51c4a52e48485adbe8c286e
asyqarilandika <asyqarilandika10@gmail.com>
2025-11-24 23:33:01 +0800

    test

--- start/routes.ts
+++ start/routes.ts
@@ -2,7 +2,7 @@ import router from '@adonisjs/core/services/router'
 
 router
   .group(() => {
-    router.resource('coordinates', () => import('#controllers/coordinates_controller')).apiOnly()
+    router.resource('/', () => import('#controllers/coordinates_controller')).apiOnly()
   })
   .prefix('/coordinate')
 router.on('/').renderInertia('bismillahimmo')
