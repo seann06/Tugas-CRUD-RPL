713d6209e64059520b56dfa640b84cc51e37dc77
asyqarilandika <asyqarilandika10@gmail.com>
2025-11-24 23:46:52 +0800

    test

--- start/routes.ts
+++ start/routes.ts
@@ -1,4 +1,5 @@
+import CoordinatesController from '#controllers/coordinates_controller'
 import router from '@adonisjs/core/services/router'
 
-router.resource('/coordinate', () => import('#controllers/coordinates_controller')).apiOnly()
+router.resource('/coordinate', CoordinatesController).apiOnly()
 router.on('/').renderInertia('bismillahimmo')
